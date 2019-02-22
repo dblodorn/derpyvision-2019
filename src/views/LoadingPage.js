482e626a69643cead59e6d62aaad00359a1bd41d
Dain Blodorn Kim <dain@WM-iMac-6.local>
2019-02-22 15:23:49 -0800

    loading wrapper

--- src/views/LoadingPage.js
+++ src/views/LoadingPage.js
@@ -3,9 +3,7 @@ import styled from 'styled-components'
 import { fullWindow, flexCenteredAll } from './../styles/mixins'
 
 export default () =>
-  <LoadingWrapper>
-    <h1>Loading</h1>
-  </LoadingWrapper>
+  <LoadingWrapper></LoadingWrapper>
 
 // STYLES
 const LoadingWrapper = styled.div`
